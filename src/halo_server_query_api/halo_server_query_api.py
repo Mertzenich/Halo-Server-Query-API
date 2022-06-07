488f9f8836a2831ecb939a291481d7ba01ae55ff
Tagia Network <tagianetwork@gmail.com>
2022-06-06 19:34:35 -0500

    Removed specific attribute request

--- src/halo_server_query_api/halo_server_query_api.py
+++ src/halo_server_query_api/halo_server_query_api.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-from haloserverquery.haloserverquery import queryServer
-from typing import Optional
+from halo_server_query import queryServer
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/query/{addr}")
-def query_server(addr: str, port: int = 2302, attr: str = None):
+def query_server(addr: str, port: int = 2302):
     """Query a halo server at addr:port"""
-    if attr:
-        return queryServer(addr, port)[attr]
-    return queryServer(addr, port)
+    query = queryServer(addr, port)
+    return query
